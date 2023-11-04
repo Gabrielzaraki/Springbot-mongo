e74a06daf34be36dc6b72f8018b7a8e8c513a5bf
Gabrielzaraki <gabriel.henrique.102@gmail.com>
2023-11-04 15:05:16 -0300

    AuthorDTO

--- src/main/java/com/work/demo/config/Instantiation.java
+++ src/main/java/com/work/demo/config/Instantiation.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.work.demo.DTO.AuthorDTO;
 import com.work.demo.domain.Post;
 import com.work.demo.domain.User;
 import com.work.demo.repository.PostRepository;
@@ -31,10 +32,12 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Quero mamar", maria);
-		Post post2 = new Post(null, sdf.parse("25/03/2018"), "Partiu Dar", "Quero sentar", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Quero mamar", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("25/03/2018"), "Partiu Dar", "Quero sentar", new AuthorDTO(maria));
+		
+	
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
 	}
