11f1354356fefb74ec15176aef7fcab4a0102f81
Gabrielzaraki <gabriel.henrique.102@gmail.com>
2023-11-04 09:47:03 -0300

    Insert user

--- src/main/java/com/work/demo/services/UserService.java
+++ src/main/java/com/work/demo/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.work.demo.DTO.UserDTO;
 import com.work.demo.domain.User;
 import com.work.demo.repository.UserRepository;
 import com.work.demo.services.exception.ObjectNotFoundException;
@@ -25,6 +26,14 @@ public class UserService {
 			throw new ObjectNotFoundException("Objeto Não encontrado");
 		}
 		return user;
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
 
 }
